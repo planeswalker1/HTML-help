72bc6533d50fda08285ec725dc3d76a37cde9a69
dan <daniel.munozdev@gmail.com>
2017-12-02 14:03:22 -0800

    Add a forEach loop to the elements array and add click event listeners to each array element

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,9 @@
 //Business (or back-end) logic:
 let elements = ['body', 'div', 'h1', 'p', 'img'];
+// user interface (or front-end) logic
+elements.forEach(function(element) {
+  $(element).click(function(event) {
+    event.stopPropagation();
+    alert('This is a ' + element + ' tag!');
+  });
+});
